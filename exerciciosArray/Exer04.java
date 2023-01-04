579e1da99e1f0cf49a16b32759f4556bdd430127
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-03 23:26:33 -0300

    treinando com arrays - exercicio04

---
+++ exerciciosArray/Exer04.java
@@ -0,0 +1,32 @@
+package exerciciosArray;
+
+public class Exer04 {
+    public static void main(String[] args) {
+
+        int[] vetorA = new int[15];
+        double[] vetorB = new double[15];
+
+        vetorA[0] = 1;
+        vetorA[1] = 2;
+        vetorA[2] = 3;
+        vetorA[3] = 4;
+        vetorA[4] = 5;
+        vetorA[5] = 6;
+        vetorA[6] = 5;
+        vetorA[7] = 4;
+        vetorA[8] = 3;
+        vetorA[9] = 2;
+        vetorA[10] = 1;
+        vetorA[11] = 2;
+        vetorA[12] = 2;
+        vetorA[13] = 2;
+        vetorA[14] = 7;
+
+        System.out.println("VetorB = VetorA ao quadrado");
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = Math.sqrt(vetorA[i]);
+            System.out.printf("VetorA = %d | Vetor B = %.2f\n", vetorA[i], vetorB[i]);
+        }
+    }
+
+}
