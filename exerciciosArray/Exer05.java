895d36e837c32d9ddd3478d2ea04ac2135118657
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-03 23:34:26 -0300

    VetorB = VetorA X índice

---
+++ exerciciosArray/Exer05.java
@@ -0,0 +1,27 @@
+package exerciciosArray;
+
+public class Exer05 {
+    public static void main(String[] args) {
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[10];
+
+        vetorA[0] = 1;
+        vetorA[1] = 2;
+        vetorA[2] = 3;
+        vetorA[3] = 4;
+        vetorA[4] = 5;
+        vetorA[5] = 6;
+        vetorA[6] = 5;
+        vetorA[7] = 4;
+        vetorA[8] = 3;
+        vetorA[9] = 2;
+
+        System.out.println("VetorB = VetorA x índice");
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = (vetorA[i] * i);
+            System.out.printf("VetorA = %d | Vetor B = %d\n", vetorA[i], vetorB[i]);
+        }
+    }
+
+}
