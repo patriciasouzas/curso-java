e9060bc76066e83cd4e31f1bfee096305e76a201
patriciasouzas <patriciasouzaa89@gmail.com>
2023-02-22 23:52:14 -0300

    conceito de matriz

---
+++ aula20/Matrizes.java
@@ -0,0 +1,16 @@
+package aula20;
+
+public class Matrizes {
+    public static void main(String[] args) {
+
+        double[][] notasAlunos = new double[3][3];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 8;
+        notasAlunos[0][2] = 7;
+
+        notasAlunos[1][0] = 7;
+        notasAlunos[1][1] = 9;
+        notasAlunos[1][2] = 8;
+    }
+}
