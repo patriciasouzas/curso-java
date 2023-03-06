25f90fc150318352919b0be1f04ca259a5df613d
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-06 19:06:30 -0300

    exercício ligar e desligar lâmpada

---
+++ exerciciosOO/aulas25a27/Exerc01.java
@@ -0,0 +1,24 @@
+package exerciciosOO.aulas25a27;
+
+import java.util.Scanner;
+
+public class Exerc01 {
+
+    public static void main(String[] args) {
+
+        String ligar;
+        Scanner input = new Scanner(System.in);
+        Lampada lampada = new Lampada();
+
+        System.out.println("Você quer ligar a lâmpada? (S - N)");
+        ligar = input.next();
+
+        if (ligar.equalsIgnoreCase("S")) {
+            lampada.ligarLampada();
+            System.out.println("A lâmpada foi ligada.");
+        } else if (ligar.equalsIgnoreCase("N")) {
+            lampada.desligarLampada();
+            System.out.println("A lâmpada foi desligada.");
+        } else System.out.println("Opção desconhecida. Tente novamente.");
+    }
+}
---
+++ exerciciosOO/aulas25a27/Lampada.java
@@ -0,0 +1,14 @@
+package exerciciosOO.aulas25a27;
+
+public class Lampada {
+
+    boolean lampada;
+
+    void ligarLampada(){
+        lampada = true;
+    }
+
+    void desligarLampada() {
+        lampada = false;
+    }
+}
