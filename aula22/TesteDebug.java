d7b8831ed914aa08e477635893b6ed2eb45948e6
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-05 23:20:13 -0300

    teste com debug

---
+++ aula22/TesteDebug.java
@@ -0,0 +1,12 @@
+package aula22;
+
+public class TesteDebug {
+    public static void main(String[] args) {
+
+        int[] notas = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            notas[i] = i;
+        }
+    }
+}
